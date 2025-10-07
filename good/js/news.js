ab8d793658d990deed9c623b5dc66a7235ac28e0
SoulShadow8326 <soulshadow8326@gmail.com>
2025-10-07 21:22:25 +0530

    added good people interface

---
+++ good/js/news.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('assets/news.json')
+        .then(function (r) { return r.json(); })
+        .then(function (data) {
+            var container = document.getElementById('news-list');
+            data.forEach(function (item) {
+                var el = document.createElement('div');
+                el.className = 'news-item';
+                el.innerHTML = '<div class="log-date">Log: ' + item.date + '</div>' +
+                    '<div class="log-body"><strong>' + escapeHtml(item.title) + '</strong><br/>' + escapeHtml(item.body) + '</div>';
+                container.appendChild(el);
+            });
+        });
+
+    function escapeHtml(s) {
+        return String(s).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    }
+});
